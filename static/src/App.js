ab7523cc5ccbf3aba49b6c1492ba2630321bbc3c
rawrbyte <rawrbyte@tamu.edu>
2018-03-26 19:27:23 -0500

    added circle links and popup

--- static/src/App.js
+++ static/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {Button} from 'reactstrap';
+import Popup from 'reactjs-popup';
 import './App.css';
 
 class App extends Component {
@@ -8,9 +10,40 @@ class App extends Component {
         <div class="header">
         <h1><img src="/img/logo.png" /> The OnePage</h1> 
         </div>
-        <div class="left">Column</div>
-        <div class="middle">Column</div>
-        <div class="right">Column</div>
+        <div class="left">
+            <Popup
+              trigger={<Button bsStyle="primary">Edit</Button>}
+              modal
+              closeOnDocumentClick>
+              <div><img src="/img/filler.svg" /></div>
+              <div><img src="/img/filler.svg" /></div>
+              <div><img src="/img/filler.svg" /></div>
+              <div><img src="/img/filler.svg" /></div>
+              <div><img src="/img/filler.svg" /></div>
+              <div><img src="/img/filler.svg" /></div>
+              <div><img src="/img/filler.svg" /></div>
+              <div><img src="/img/filler.svg" /></div>
+            </Popup>
+            <div class="circleLinks">
+              <img src="/img/filler.svg" />
+              <img src="/img/filler.svg" />
+              <img src="/img/filler.svg" />
+              <img src="/img/filler.svg" />
+            </div>
+            <div class="circleLinks">
+              <img src="/img/filler.svg" />
+              <img src="/img/filler.svg" />
+              <img src="/img/filler.svg" />
+              <img src="/img/filler.svg" />
+            </div>
+        </div>
+        <div class="middle">
+      
+        </div>
+        <div class="right">
+        
+        
+        </div>
         <div class="footer">
           <p>Footer</p>
         </div>
