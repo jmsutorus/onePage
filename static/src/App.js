1e160e4ae5b3b174a625b1551971b76a86711782
Jose Andrade <joseluiand@tamu.edu>
2018-04-20 22:04:50 -0500

    Added new row of links; added banners to feeds

--- static/src/App.js
+++ static/src/App.js
@@ -9,11 +9,9 @@ class App extends Component {
 		return (
 			<div>
 				<div class="header">
+					{/*(<iframe src="http://free.timeanddate.com/clock/i6785m90/n2284/th2" frameborder="0" width="114" height="18"></iframe><br/> */}
 					<img src="/img/logo.png" className="logo"/>
-					<img src="/img/theOnePage.png" />
-					
-					<iframe src="http://free.timeanddate.com/clock/i6785m90/n2284/th2" frameborder="0" width="114" height="18"></iframe>
-					
+					<img src="/img/theOnePage.png" className="textLogo"/>
 					{/*<div className="social-button-container">
 						<button className="social-button social-button-facebook">
 							Facebook
@@ -39,28 +37,56 @@ class App extends Component {
 						
 						<div class="circleLinks2">
 							<a href="https://www.reddit.com" target="_blank"><img src="/img/reddit_logo.png"/></a>
-							<a href="https://www.twitch.tv/" target="_blank"><img src="/img/twitch_logo.png"/></a>
+							<a href="https://www.twitch.tv" target="_blank"><img src="/img/twitch_logo.png"/></a>
 							<a href="https://www.youtube.com" target="_blank"><img src="/img/youtube_logo.png"/></a>
 							<a href="https://www.instagram.com" target="_blank"><img src="/img/ig_logo.png"/></a>
 						</div>
 						
+						<div class="circleLinks3">
+							<a href="https://howdy.tamu.edu" target="_blank"><img src="/img/howdy_logo.png"/></a>
+							<a href="https://ecampus.tamu.edu" target="_blank"><img src="/img/ecampus_logo.png"/></a>
+							<a href="https://www.amazon.com" target="_blank"><img src="/img/amazon_logo.png"/></a>
+							<a href="https://www.netflix.com" target="_blank"><img src="/img/netflix_logo.png"/></a>
+						</div>
+						
 						<div class="searchBar">
 							<form action="http://www.google.com/search" class="searchform" method="get" name="searchform" target="_blank">
-								<input autocomplete="on" class="form-controls search" name="q" placeholder="Google Search" required="required"  type="text"></input>
+								<input autocomplete="on" title="Google Search" class="form-controls search" name="q" required="required"  type="text"></input>
 							</form>
 						</div>
 					</div>
 					
-					<div class="column">
-						<div class="fb-page" data-width="350px" data-height="500px" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
-					</div>
+					<div class="column"></div>
 					
 					<div class="column">
-						<a class="twitter-timeline" data-width="350" data-height="500" data-theme="light" href="https://twitter.com/TwitterDev" data-chrome="nofooter"></a> 
+						<div class="row">
+							<img src="img/fb_banner.png"/>
+						</div>
+						<div class="row">
+							<div class="fb-page" data-width="330px" data-height="500px" data-href="https://www.facebook.com/StudentProblems/" data-tabs="timeline" data-small-header="true" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
+						</div>
 					</div>
 					
+					<div class="column"></div>
+					
+					<div class="column" >
+						<div class="row">
+							<img src="img/twitter_banner.png"/>
+						</div>
+						<div class="row">
+							<a class="twitter-timeline" data-width="350" data-height="500" data-theme="light" href="https://twitter.com/reveille" data-chrome="noheader nofooter noborders"></a> 
+						</div>
+					</div>
+					
+					<div class="column"></div>
+					
 					<div class="column">
-						<a class="twitter-timeline" data-width="350" data-height="500" data-theme="light" href="https://twitter.com/cnnbrk" data-chrome="nofooter"></a> 
+						<div class="row">
+							<img src="img/cnn_banner.png"/>
+						</div>
+						<div class="row">
+							<a class="twitter-timeline" data-width="350" data-height="500" data-theme="light" href="https://twitter.com/cnnbrk" data-chrome="noheader nofooter noborders"></a> 
+						</div>
 					</div>
 					
 					
